8f95db5e68bc298addc9f7a01a69e898f86db980
fuhad saneen <saneenfuhad81@gmail.com>
2025-04-22 11:58:05 +0530

    Add files via upload

---
+++ Task.js
@@ -0,0 +1,65 @@
+// // Write a function to find the longest common prefix string amongst an array of
+// // strings.
+// // If there is no common prefix, return an empty string "".
+// // Example 1:
+// // Input: strs = ["flower","flow","flight"]
+// // Output: "fl"
+// // Example 2:
+// // Input: strs = ["dog","racecar","car"]
+// // Output: ""
+// // Explanation: There is no common prefix among the input strings.
+
+
+
+
+
+
+
+
+
+
+// // let  strs = ["flower","flow","flight"]
+
+
+// // function Task(strs){
+// //     if (!strs.length)
+// //          return ""
+   
+// // }
+
+// // console.log(Task(strs))
+
+
+
+
+
+
+
+
+
+
+// // let strs=["flower","flow","flight"]
+
+// // // let sp=strs.map((a)=> a.split(''))
+// // let ab=strs.indexOf("")
+
+// // console.log(ab)
+
+// // // let res=strs.filter((a,b)=>a.split('').includes(b))
+// // // console.log(res)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ second.js
@@ -0,0 +1,13 @@
+
+
+// let a=arr.filter(a=>arr.indexOf(a)===arr.lastIndexOf(a))
+// let res=a.reduce((b,c)=>b+c,0)
+// console.log(res)
+
+let arr=[2,9,5,3,1,4,7]
+
+let a=arr.sort((a,b)=>b-a)
+// let b=a.length-2
+
+
+console.log(a[1])
